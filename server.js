ba2f937c1a92ad8c61b67d39fc6d1cbf11ba8e8d
onopod <onopod@google.com>
2024-11-21 23:37:32 +0900

    新規登録とログインapi作成

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const authRoutes = require("./routers/auth");
 
 const PORT = 5000;
 
-
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
