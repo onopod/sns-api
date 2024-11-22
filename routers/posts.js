6d09df2ab9da862d06098d2b6c5c4a6b199e0f45
onopod <onopod@google.com>
2024-11-22 10:27:38 +0900

    /api/posts/postの疎通確認

---
+++ routers/posts.js
@@ -0,0 +1,43 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+const jwt = require("jsonwebtoken");
+
+const router = require("express").Router();
+
+// 呟き投稿API
+router.post("/post", async (req, res) => {
+    const { content } = req.body;
+
+    if (!content) {
+        return res.status(400).json({
+            "message": "投稿内容がありません"
+        });
+    }
+
+    try {
+        const newPost = await prisma.post.create({
+            data: {
+                content,
+                authorId: 7
+            }
+        });
+
+        res.status(201).json(newPost);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            "message": "サーバーエラーです。"
+        })
+    }
+    const user = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashedPassword
+        }
+    })
+    return res.json({ user });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const authRoutes = require("./routers/auth");
+const postsRoutes = require("./routers/posts");
 const cors = require("cors");
 
 const PORT = 5000;
@@ -10,5 +11,6 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/posts", postsRoutes);
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
