53f056ff3dc346ba63e7739cdbaa318120ee8d0b
onopod <onopod@google.com>
2024-11-22 09:17:41 +0900

    server_add

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const authRoutes = require("./routers/auth");
+const cors = require("cors");
 
 const PORT = 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
